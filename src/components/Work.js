d235dad4c76c09b0b858cda3683fa8156dbe9abb
fazleh-rakib <rakibfazleh@gmail.com>
2024-01-18 16:29:12 +0600

    implimenting work done

--- src/components/Work.js
+++ src/components/Work.js
@@ -12,13 +12,17 @@ const Work = () => {
     <div className="section" id="work">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row gap-x-10">
-          <div className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
+          <motion.div
+          variants={fadeIn("down", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
             {/* text */}
             <div>
               <h2 className="h2 leading-tight text-accent">
                 My Laatest <br /> Work.
               </h2>
-              <p className="max-w-sm mb-16">
+              <p className="max-w-sm mb-14">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Eveniet voluptatum explicabo sit asperiores, officia recusandae
                 beatae delectus accusamus accusantium neque!
@@ -27,7 +31,10 @@ const Work = () => {
             </div>
             {/* img */}
             <div className="group relative overflow-hidden border-2 border-white/20 rounded-xl">
-              <div>
+              <motion.div  
+              variants={fadeIn("left", 0.3)}
+              initial="hidden"
+              whileInView={"show"}>
                 {/* overlay */}
                 <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-500"></div>
                 <img
@@ -41,12 +48,66 @@ const Work = () => {
                 </div>
                 {/*  title */}
                 <div className="absolute -bottom-full left-12 group-hover:bottom-14 z-50 transition-all duration-800">
-                  <span className="text-white text-3xl ">Project Title</span>
+                  <span className="text-white text-3xl ">
+                    Portfolio Template
+                  </span>
                 </div>
-              </div>
+              </motion.div>
             </div>
-          </div>
-          <div>2</div>
+          </motion.div>
+          {/* 2nd Div */}
+          <motion.div 
+          variants={fadeIn("up", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          className="flex-1 flex flex-col gap-y-10">
+            <div className="group relative overflow-hidden border-2 border-white/20 rounded-xl">
+              <motion.div
+                variants={fadeIn("left", 0.3)}
+                initial="hidden"
+                whileInView={"show"}
+              >
+                {/* overlay */}
+                <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-500"></div>
+                <img
+                  className="group-hover:scale-125 transition-all duration-300"
+                  src={img2}
+                  alt=""
+                />
+                {/* pre title */}
+                <div className="absolute -bottom-full left-12 group-hover:bottom-24 z-50 transition-all duration-500">
+                  <span className="text-gradient font-bold">UI/UX Designe</span>
+                </div>
+                {/*  title */}
+                <div className="absolute -bottom-full left-12 group-hover:bottom-14 z-50 transition-all duration-800">
+                  <span className="text-white text-3xl ">Virtual Reality</span>
+                </div>
+              </motion.div>
+            </div>
+            <div className="group relative overflow-hidden border-2 border-white/20 rounded-xl">
+              <motion.div
+                variants={fadeIn("right", 0.3)}
+                initial="hidden"
+                whileInView={"show"}
+              >
+                {/* overlay */}
+                <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-500"></div>
+                <img
+                  className="group-hover:scale-125 transition-all duration-300"
+                  src={img3}
+                  alt=""
+                />
+                {/* pre title */}
+                <div className="absolute -bottom-full left-12 group-hover:bottom-24 z-50 transition-all duration-500">
+                  <span className="text-gradient font-bold">UI/UX Designe</span>
+                </div>
+                {/*  title */}
+                <div className="absolute -bottom-full left-12 group-hover:bottom-14 z-50 transition-all duration-800">
+                  <span className="text-white text-3xl ">Rock & Role</span>
+                </div>
+              </motion.div>
+            </div>
+          </motion.div>
         </div>
       </div>
     </div>
